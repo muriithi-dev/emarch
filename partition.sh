ac0149d515406f0fb923681e5fc5b41a19bd22a7
D. Muriithi <muriithi.wanderi.daniel@gmail.com>
2026-06-08 17:48:40 +0300

    Refactor partitioning script for improved clarity

--- partition.sh
+++ partition.sh
@@ -3,46 +3,69 @@ set -e
 
 DISK="/dev/sda"
 
+#########################
+# SIZE VARIABLES (GB)
+#########################
 EFI_GB=1
 SWAP_GB=2
 
+#########################
+# CONVERT GB → MiB
+#########################
 GB_TO_MIB=1024
 
 EFI_MIB=$((EFI_GB * GB_TO_MIB))
 SWAP_MIB=$((SWAP_GB * GB_TO_MIB))
 
+#########################
+# START POSITION
+#########################
 START=1
 
-#########################
-# PARTITIONING
-#########################
-
+echo "Creating GPT on $DISK"
 parted -s "$DISK" mklabel gpt
 
-# EFI
+#########################
+# PART 1 - EFI
+#########################
 END=$((START + EFI_MIB))
 parted -s "$DISK" mkpart ESP fat32 ${START}MiB ${END}MiB
 parted -s "$DISK" set 1 esp on
 EFI_PART=1
 START=$END
 
-# SWAP
+#########################
+# PART 2 - SWAP
+#########################
 END=$((START + SWAP_MIB))
 parted -s "$DISK" mkpart swap linux-swap ${START}MiB ${END}MiB
 SWAP_PART=2
 START=$END
 
-# ROOT (rest of disk)
+#########################
+# PART 3 - ROOT (REST)
+#########################
 parted -s "$DISK" mkpart root btrfs ${START}MiB 100%
 ROOT_PART=3
 
-#########################
-# FORMAT
-#########################
+echo "Partitioning done"
+parted -s "$DISK" print
 
+#########################
+# FORMATTING
+#########################
+echo "Formatting partitions..."
+
+# EFI (FAT32)
 mkfs.fat -F32 "${DISK}${EFI_PART}"
+
+# SWAP
 mkswap "${DISK}${SWAP_PART}"
+
+# ROOT
 mkfs.btrfs -f "${DISK}${ROOT_PART}"
+
+echo "Formatting complete"
 
 #########################
 # CREATE SUBVOLUMES
@@ -61,10 +84,11 @@ umount /mnt
 # MOUNT
 #########################
 
+# mount "${DISK}${ROOT_PART}" -o subvolid=256 /mnt
 mount -o subvol=@,compress=zstd:3,noatime \
     "${DISK}${ROOT_PART}" /mnt
 
-mkdir -p /mnt/{boot,home,.snapshots,var/log}
+mkdir -p /mnt/{home,.snapshots,efi,.btrfsroot,var/log}
 
 mount -o subvol=@home,compress=zstd:3,noatime \
     "${DISK}${ROOT_PART}" /mnt/home
@@ -80,6 +104,9 @@ mount "${DISK}${EFI_PART}" /mnt/boot
 swapon "${DISK}${SWAP_PART}"
 
 echo "Ready for pacstrap"
+
+
+
 
 
 
