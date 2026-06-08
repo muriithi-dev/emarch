017c2041b1b9b7962689c296ade8f685339cf2f2
D. Muriithi <muriithi.wanderi.daniel@gmail.com>
2026-06-08 17:49:48 +0300

    Refactor partitioning script for clarity and updates

--- partition.sh
+++ partition.sh
@@ -3,69 +3,46 @@ set -e
 
 DISK="/dev/sda"
 
-#########################
-# SIZE VARIABLES (GB)
-#########################
 EFI_GB=1
 SWAP_GB=2
 
-#########################
-# CONVERT GB → MiB
-#########################
 GB_TO_MIB=1024
 
 EFI_MIB=$((EFI_GB * GB_TO_MIB))
 SWAP_MIB=$((SWAP_GB * GB_TO_MIB))
 
-#########################
-# START POSITION
-#########################
 START=1
 
-echo "Creating GPT on $DISK"
+#########################
+# PARTITIONING
+#########################
+
 parted -s "$DISK" mklabel gpt
 
-#########################
-# PART 1 - EFI
-#########################
+# EFI
 END=$((START + EFI_MIB))
 parted -s "$DISK" mkpart ESP fat32 ${START}MiB ${END}MiB
 parted -s "$DISK" set 1 esp on
 EFI_PART=1
 START=$END
 
-#########################
-# PART 2 - SWAP
-#########################
+# SWAP
 END=$((START + SWAP_MIB))
 parted -s "$DISK" mkpart swap linux-swap ${START}MiB ${END}MiB
 SWAP_PART=2
 START=$END
 
-#########################
-# PART 3 - ROOT (REST)
-#########################
+# ROOT (rest of disk)
 parted -s "$DISK" mkpart root btrfs ${START}MiB 100%
 ROOT_PART=3
 
-echo "Partitioning done"
-parted -s "$DISK" print
-
 #########################
-# FORMATTING
+# FORMAT
 #########################
-echo "Formatting partitions..."
 
-# EFI (FAT32)
 mkfs.fat -F32 "${DISK}${EFI_PART}"
-
-# SWAP
 mkswap "${DISK}${SWAP_PART}"
-
-# ROOT
 mkfs.btrfs -f "${DISK}${ROOT_PART}"
-
-echo "Formatting complete"
 
 #########################
 # CREATE SUBVOLUMES
@@ -84,11 +61,10 @@ umount /mnt
 # MOUNT
 #########################
 
-# mount "${DISK}${ROOT_PART}" -o subvolid=256 /mnt
 mount -o subvol=@,compress=zstd:3,noatime \
     "${DISK}${ROOT_PART}" /mnt
 
-mkdir -p /mnt/{home,.snapshots,efi,.btrfsroot,var/log}
+mkdir -p /mnt/{boot,home,.snapshots,var/log}
 
 mount -o subvol=@home,compress=zstd:3,noatime \
     "${DISK}${ROOT_PART}" /mnt/home
@@ -104,10 +80,3 @@ mount "${DISK}${EFI_PART}" /mnt/boot
 swapon "${DISK}${SWAP_PART}"
 
 echo "Ready for pacstrap"
-
-
-
-
-
-
-
