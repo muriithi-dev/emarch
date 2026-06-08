5b0d6ec246e005dd6c036d594c5e3fe4654147f3
D. Muriithi <muriithi.wanderi.daniel@gmail.com>
2026-06-08 13:09:08 +0300

    Refactor partition script for improved clarity and safety

--- partition.sh
+++ partition.sh
@@ -1,37 +1,120 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-target="/dev/sda"
+############################
+# CONFIG
+############################
 
-if [[ ! -b "$target" ]]; then
-    echo "Error: $target is not a block device"
-    exit 1
+DISK="/dev/sda"
+
+EFI_SIZE="1GiB"
+SWAP_SIZE="6GiB"
+
+HOSTNAME="arch"
+
+LUKS_NAME="cryptroot"
+MAPPER_ROOT="/dev/mapper/cryptroot"
+
+############################
+# SAFETY CHECK
+############################
+
+if [[ ! -b "$DISK" ]]; then
+  echo "ERROR: $DISK is not a block device"
+  exit 1
 fi
 
-cat /sys/firmware/efi/fw_platform_size
+echo "!!! THIS WILL WIPE $DISK !!!"
+read -rp "Type YES to continue: " confirm
+[[ "$confirm" == "YES" ]] || exit 1
 
-fdisk "$target" <<'EOF'
-g
-n
-1
+############################
+# PARTITIONING (parted)
+############################
 
-+1G
-t
-1
-n
-2
+echo "Creating partitions..."
 
-+6G
-t
-2
-19
-n
-3
+parted -s "$DISK" mklabel gpt
 
+parted -s "$DISK" mkpart ESP fat32 1MiB "$EFI_SIZE"
+parted -s "$DISK" set 1 esp on
 
-w
-EOF
+parted -s "$DISK" mkpart swap linux-swap "$EFI_SIZE" "$((6 + 1))GiB"
 
-mkfs.fat -F32 "${target}1"
-mkswap "${target}2"
-mkfs.ext4 "${target}3"
+parted -s "$DISK" mkpart luks ext4 "$((6 + 1))GiB" 100%
+
+############################
+# DETECT PARTITION PREFIX
+############################
+
+if [[ "$DISK" =~ nvme|mmcblk ]]; then
+  P="p"
+else
+  P=""
+fi
+
+EFI_PART="${DISK}${P}1"
+SWAP_PART="${DISK}${P}2"
+ROOT_PART="${DISK}${P}3"
+
+############################
+# EFI FORMAT
+############################
+
+mkfs.fat -F32 "$EFI_PART"
+
+############################
+# LUKS SETUP (root only)
+############################
+
+echo "Setting up LUKS..."
+cryptsetup luksFormat "$ROOT_PART"
+
+cryptsetup open "$ROOT_PART" "$LUKS_NAME"
+
+############################
+# BTRFS SETUP
+############################
+
+mkfs.btrfs -L "arch_root" "$MAPPER_ROOT"
+
+mount "$MAPPER_ROOT" /mnt
+
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
+
+umount /mnt
+
+############################
+# MOUNT SUBVOLUMES
+############################
+
+mount -o subvol=@,compress=zstd "$MAPPER_ROOT" /mnt
+
+mkdir -p /mnt/{boot,home,.snapshots}
+
+mount -o subvol=@home,compress=zstd "$MAPPER_ROOT" /mnt/home
+mount -o subvol=@snapshots,compress=zstd "$MAPPER_ROOT" /mnt/.snapshots
+
+mount "$EFI_PART" /mnt/boot
+
+############################
+# SWAP
+############################
+
+mkswap "$SWAP_PART"
+swapon "$SWAP_PART"
+
+############################
+# FSTAB GENERATION
+############################
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+############################
+# DONE (BASE SYSTEM READY)
+############################
+
+echo "Base system mounted at /mnt"
+echo "Continue with pacstrap + arch-chroot"
